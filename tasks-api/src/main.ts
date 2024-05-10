8651513fab0f26dfc5e72418012228fe9fb98632
Alex Melo <afabiano15@gmail.com>
2024-05-10 19:09:48 +0000

    swagger configuration

--- tasks-api/src/main.ts
+++ tasks-api/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const options = new DocumentBuilder()
+    .setTitle('Tasks API')
+    .setDescription('API for task management')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs', app, document);
   await app.listen(3000);
 }
+
 bootstrap();
